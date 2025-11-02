6651ea83cc8c54c95ac7f3841f32bae90b5bdd6b
Vanya A. Sergeev <v@sergeev.io>
2025-11-01 19:46:30 -0500

    tests/i2c: add eeprom write/read to loopback test

--- tests/test_i2c.lua
+++ tests/test_i2c.lua
@@ -10,6 +10,8 @@ local I2C = periphery.I2C
 
 --------------------------------------------------------------------------------
 
+local I2C_EEPROM_ADDRESS    = 0x51
+
 local device = nil
 
 --------------------------------------------------------------------------------
@@ -45,7 +47,60 @@ function test_loopback()
 
     ptest()
 
-    print("No general way to do a loopback test for I2C without a real component, skipping...")
+    -- Open i2c device
+    passert_periphery_success("open i2c", function () i2c = I2C(device) end)
+
+    -- Generate random byte vector
+    local vector_table = {}
+    local vector_str = ""
+    math.randomseed(1234)
+    for i=1, 32 do
+        local b = math.random(0, 255)
+        vector_table[i] = b
+        vector_str = vector_str .. string.char(b)
+    end
+
+    -- Write bytes to 0x100 with table
+    -- S [ 0x51 W ] [ 0x01 ] [ 0x00 ] [ Data... ] P
+    local msgs = { { 0x01, 0x00 } }
+    for i=1, #vector_table do msgs[1][2 + i] = vector_table[i] end
+    passert_periphery_success("write bytes to 0x100", function () i2c:transfer(I2C_EEPROM_ADDRESS, msgs) end)
+
+    -- Wait for Write Cycle
+    periphery.sleep_ms(10)
+
+    -- Read bytes from 0x100 with table
+    -- S [ 0x51 W ] [ 0x01 ] [ 0x00 ] S [ 0x51 R ] [ Data... ] P
+    local msgs = { { 0x01, 0x00 }, { flags = I2C.I2C_M_RD } }
+    for i=1, #vector_table do msgs[2][i] = 0x00 end
+    passert_periphery_success("read bytes from 0x100", function () i2c:transfer(I2C_EEPROM_ADDRESS, msgs) end)
+
+    -- Verify bytes
+    for i=1, #vector_table do
+        if msgs[2][i] ~= vector_table[i] then
+            pfail(string.format("mismatch on index %d. expected 0x%02x, got 0x%02x", i, vector_table[i], msgs[2][i]))
+            os.exit(1)
+        end
+    end
+
+    -- Write bytes to 0x200 with string
+    -- S [ 0x51 W ] [ 0x02 ] [ 0x00 ] [ Data... ] P
+    local msgs = { { string.char(0x02) .. string.char(0x00) .. vector_str } }
+    passert_periphery_success("write bytes to 0x200", function () i2c:transfer(I2C_EEPROM_ADDRESS, msgs) end)
+
+    -- Wait for Write Cycle
+    periphery.sleep_ms(10)
+
+    -- Read bytes from 0x200 with string
+    -- S [ 0x51 W ] [ 0x02 ] [ 0x00 ] S [ 0x51 R ] [ Data... ] P
+    local msgs = { { string.char(0x02) .. string.char(0x00) }, { string.rep(string.char(0x00), #vector_str), flags = I2C.I2C_M_RD } }
+    passert_periphery_success("read bytes from 0x200", function () i2c:transfer(I2C_EEPROM_ADDRESS, msgs) end)
+
+    -- Verify bytes
+    passert("verify bytes", msgs[2][1] == vector_str)
+
+    -- Close i2c device
+    passert_periphery_success("close i2c", function () i2c:close() end)
 end
 
 function test_interactive()
@@ -85,7 +140,7 @@ if #arg < 1 then
     io.stderr:write(string.format("Usage: lua %s <I2C device>\n\n", arg[0]))
     io.stderr:write("[1/4] Arguments test: No requirements.\n")
     io.stderr:write("[2/4] Open/close test: I2C device should be real.\n")
-    io.stderr:write("[3/4] Loopback test: No test.\n")
+    io.stderr:write("[3/4] Loopback test: Expects 24XX32 EEPROM (or similar) at address 0x51.\n")
     io.stderr:write("[4/4] Interactive test: I2C bus should be observed with an oscilloscope or logic analyzer.\n\n")
     io.stderr:write("Hint: for Raspberry Pi 3, enable I2C1 with:\n")
     io.stderr:write("   $ echo \"dtparam=i2c_arm=on\" | sudo tee -a /boot/config.txt\n")
